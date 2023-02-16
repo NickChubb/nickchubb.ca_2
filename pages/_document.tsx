3793542d15e42994b30dfcbd7cee8b0df93ec89d
NickChubb <nick@nickchubb.ca>
2023-02-15 17:53:58 -0800

    add _document page

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <script>0</script>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
