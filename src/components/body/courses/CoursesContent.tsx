6bf8a68c4a513d8212657a42823133d201a24a44
NickChubb <nick@nickchubb.ca>
2023-02-17 20:56:09 -0800

    course list mobile full width

--- src/components/body/courses/CoursesContent.tsx
+++ src/components/body/courses/CoursesContent.tsx
@@ -5,10 +5,16 @@ import CourseListItem from "./CourseListItem"
 import { CourseItem } from "./CourseTypes"
 import { courseData } from "../../../data/courses"
 import 'react-accessible-accordion/dist/fancy-example.css'
+import { breakpoints } from "../../shared/styles"
 
 const CourseListWrapper = styled(Accordion)`
   border: 1px solid rgba(0, 0, 0, 0.1);
   border-radius: 2px;
+
+  @media only screen and (max-width: ${breakpoints.mobile}) {
+    border: none;
+    margin: 0 -2rem;
+  }
 `
 
 const CoursesContent: React.FC = () => {
