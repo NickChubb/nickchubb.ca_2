4d9e731869bd8ada98fa2896077347b775ed4b87
NickChubb <nick@nickchubb.ca>
2023-10-11 22:46:50 +0800

    Update mobile map

--- src/components/body/personal/map/index.tsx
+++ src/components/body/personal/map/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps'
-import { text } from '../../../shared/styles'
+import { breakpoints, text } from '../../../shared/styles'
 import { includes, propSatisfies } from 'ramda'
 import { visited } from './data'
 import map from './map.json'
@@ -10,24 +10,32 @@ const geoUrl = map
 
 const MapSectionWrapper = styled.div``
 
+const MapWrapper = styled.div`
+  @media only screen and (max-width: ${breakpoints.mobile}) {
+    margin: 0 -36px;
+  }
+`
+
 const MapSection: React.FC<{}> = () => {
   return (
     <MapSectionWrapper>
       <div>Travel is a big part of my life</div>
       <div>Countries Visited: {visited.length}/195</div>
-      <ComposableMap projection="geoMercator" width={1200} height={900}>
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              console.log(geo.properties.name)
-              const fill = includes(geo.properties.name, visited)
-                ? text.green
-                : 'white'
-              return <Geography fill={fill} key={geo.rsmKey} geography={geo} />
-            })
-          }
-        </Geographies>
-      </ComposableMap>
+      <MapWrapper>
+        <ComposableMap projection="geoMercator" width={1200} height={900}>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => {
+                console.log(geo.properties.name)
+                const fill = includes(geo.properties.name, visited)
+                  ? text.green
+                  : 'white'
+                return <Geography fill={fill} key={geo.rsmKey} geography={geo} />
+              })
+            }
+          </Geographies>
+        </ComposableMap>
+      </MapWrapper>
     </MapSectionWrapper>
   )
 }
