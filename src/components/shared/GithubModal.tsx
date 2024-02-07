328f854478919bebe54bc20171f0cc112266ce34
NickChubb <nick@nickchubb.ca>
2024-02-07 12:55:30 -0800

    Add floating-ui

--- src/components/shared/GithubModal.tsx
+++ src/components/shared/GithubModal.tsx
@@ -1,18 +1,22 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { FaGithub } from 'react-icons/fa'
 import { AiFillCloseCircle } from 'react-icons/ai'
 import styled from 'styled-components'
-import { breakpoints, colour, text } from './styles'
+import { breakpoints, colour, shadow, text } from './styles'
 import useMediaQuery from '../../hooks/use-media-query'
-
-const Wrapper = styled.div`
-  position: fixed;
-  top: 32px;
-  right: 48px;
-  display: flex;
-  gap: 12px;
-  font-size: 24px;
-`
+import {
+  useFloating,
+  autoUpdate,
+  offset,
+  shift,
+  inline,
+  useHover,
+  useFocus,
+  useDismiss,
+  useRole,
+  useInteractions,
+  FloatingArrow
+} from '@floating-ui/react'
 
 const Popup = styled.div`
   background: ${colour.cardHeader};
@@ -25,6 +29,7 @@ const Popup = styled.div`
   border: 1px solid ${colour.cardBackground};
   font-size: 14px;
   user-select: none;
+  box-shadow: ${shadow.tooltip};
 `
 
 const PopupButton = styled.span`
@@ -43,6 +48,10 @@ const ModalLink = styled.a<{ fade: boolean }>`
   color: white;
   opacity: 0.5;
   transition: opacity 0.25s;
+  position: fixed;
+  top: 32px;
+  right: 48px;
+  font-size: 24px;
 
   ${(props) =>
     !props.fade &&
@@ -65,9 +74,10 @@ type GithubModalPops = {
 }
 
 const GithubModal: React.FC<GithubModalPops> = ({ section }) => {
-  const [showPopup, setShowPopup] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
   const [triggeredPopup, setTriggered] = useState(false)
   const isMobile = useMediaQuery(`(max-width: ${breakpoints.mobile})`)
+  if (isMobile) return null
 
   const link = useMemo(() => {
     // volunteering section usees same component as experience
@@ -75,36 +85,69 @@ const GithubModal: React.FC<GithubModalPops> = ({ section }) => {
     return `https://github.com/NickChubb/nickchubb.ca_2/tree/main/src/components/body/${slug}`
   }, [section])
 
+  const arrowRef = useRef(null);
+  const { refs, floatingStyles, context } = useFloating({
+    placement: 'left',
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    middleware: [
+      offset(10),
+      shift(),
+      inline()
+    ],
+    whileElementsMounted: autoUpdate,
+  })
+
+  const hover = useHover(context, { move: false })
+  const focus = useFocus(context)
+  const dismiss = useDismiss(context)
+  const role = useRole(context, {
+    role: 'tooltip',
+  })
+
+  // Merge all the interactions into prop getters
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    hover,
+    focus,
+    dismiss,
+    role,
+  ])
+
   useEffect(() => {
     if (section === 'experience' && triggeredPopup === false) {
-      setShowPopup(true)
+      setIsOpen(true)
       setTriggered(true)
     }
   }, [section])
 
-  if (isMobile) return null
-
   return (
-    <Wrapper>
-      {showPopup && (
-        <Popup>
+    <>
+      {isOpen && (
+        <Popup
+          ref={refs.setFloating}
+          style={floatingStyles}
+          {...getFloatingProps()}
+        >
           <PopupButton>
-            <AiFillCloseCircle onClick={() => setShowPopup(false)} />
+            <AiFillCloseCircle onClick={() => setIsOpen(false)} />
           </PopupButton>
           <PopupContent>
             Click this button to explore the current section on Github
           </PopupContent>
           <span>👉</span>
+          <FloatingArrow ref={arrowRef} context={context} fill={colour.cardHeader} />
         </Popup>
       )}
-      <ModalLink href={link} target="_blank" fade={!showPopup}>
-        <FaGithub
-          width="32px"
-          height="32px"
-          onClick={() => setShowPopup(false)}
-        />
+      <ModalLink
+        href={link}
+        target="_blank"
+        fade={!isOpen}
+        ref={refs.setReference}
+        {...getReferenceProps()}
+      >
+        <FaGithub width="32px" height="32px" onClick={() => setIsOpen(false)} />
       </ModalLink>
-    </Wrapper>
+    </>
   )
 }
 
