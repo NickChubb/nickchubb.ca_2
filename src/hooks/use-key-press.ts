07580aa0bf5d6fef4a13c56e31e83799f56ee5fa
NickChubb <nick@nickchubb.ca>
2024-02-12 15:56:38 -0800

    Fix fab text color bug on meta key press

--- src/hooks/use-key-press.ts
+++ src/hooks/use-key-press.ts
@@ -1,17 +1,24 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
 const useKeyPress = (key: string) => {
   const [isActive, setActive] = useState(false)
 
-  const handleKeyDown = (event: KeyboardEvent) => event.key === key && setActive(true)
-  const handleKeyUp = (event: KeyboardEvent) => event.key === key && setActive(false)
+  const handleKeyDown = (e: KeyboardEvent) => e.key === key && setActive(true)
+  const handleKeyUp = (e: KeyboardEvent) => e.key === key && setActive(false)
+  
+  // set the state to inactive when the page is blurred
+  // prevents from getting stuck in an active state if the key is 
+  // released the page is no longer in focus.
+  const handleBlur = () => setActive(false)
 
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown)
     window.addEventListener('keyup', handleKeyUp)
+    window.addEventListener('blur', handleBlur)
     return () => {
       window.removeEventListener('keydown', handleKeyDown)
-      window.addEventListener('keyup', handleKeyUp)
+      window.removeEventListener('keyup', handleKeyUp)
+      window.removeEventListener('blur', handleBlur)
     }
   }, [])
 
